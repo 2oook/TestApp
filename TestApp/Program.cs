8ce8eb40debe05f31f0f0212d686c570b71cb389
ilya petrov <pil@etz-vektor.ru>
2021-03-10 16:25:30 +0400

    Подключен веб-сервис

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -17,7 +17,7 @@ namespace TestApp
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[]
             {
-                new Service1()
+                new Service()
             };
             ServiceBase.Run(ServicesToRun);
         }
